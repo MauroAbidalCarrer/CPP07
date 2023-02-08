db088c6b81ae2e132fc0d983a042a83aeb9fb4e7
Mauro ABIDAL CARRER <mauroabidal@yahoo.fr>
2023-02-08 18:35:24 +0100

    more tests

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -28,35 +28,53 @@ int main()
     std::cout << "copied string array:" << std::endl;
     Array<std::string> copiedStrArray = Array<std::string>(strArray);
     for (size_t i = 0; i < NB_STRINGS; i++)
+        std::cout << strArray[i];
+    std::cout << std::endl;
+
+    std::cout << "modifying copy array element at index 2:" << std::endl;
+    copiedStrArray[2] = "modified string\n";
+    for (size_t i = 0; i < NB_STRINGS; i++)
+    {
+        std::cout << "original: " << strArray[i];
+        std::cout << "copy: " << copiedStrArray[i];
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
+    
+    std::cout << "assigned string array to copied Array:" << std::endl;
+    Array<std::string> assignedStrArray = Array<std::string>(NB_STRINGS);
+    assignedStrArray = copiedStrArray;
+    for (size_t i = 0; i < NB_STRINGS; i++)
     {
         std::cout << strArray[i];
     }
     std::cout << std::endl;
-    
-    std::cout << "assigned string array:" << std::endl;
-    Array<std::string> assignedStrArray = Array<std::string>(NB_STRINGS);
-    assignedStrArray = copiedStrArray;
-    // for (size_t i = 0; i < NB_STRINGS; i++)
-    // {
-    //     std::cout << strArray[i];
-    // }
-    // std::cout << std::endl;
 
-    // std::cout << "trying to access array element outside of bounds" << std::endl;
-    // try
-    // {
-    //     std::cout << strArray[NB_STRINGS];
-    // }
-    // catch(const std::exception& e)
-    // {
-    //     std::cout << "caught an error as expected: " << e.what() << '\n';
-    // }
-    // try
-    // {
-    //     std::cout << strArray[-1];
-    // }
-    // catch(const std::exception& e)
-    // {
-    //     std::cout << "caught an error as expected: " << e.what() << '\n';
-    // }
+    std::cout << "modifying assigned array element at index 3:" << std::endl;
+    assignedStrArray[3] = "another modified string\n";
+    for (size_t i = 0; i < NB_STRINGS; i++)
+    {
+        std::cout << "copied: " << copiedStrArray[i];
+        std::cout << "assigned: " << assignedStrArray[i];
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
+
+    std::cout << "trying to access array element outside of bounds" << std::endl;
+    try
+    {
+        std::cout << strArray[NB_STRINGS];
+    }
+    catch(const std::exception& e)
+    {
+        std::cout << "caught an error as expected: " << e.what() << '\n';
+    }
+    try
+    {
+        std::cout << strArray[-1];
+    }
+    catch(const std::exception& e)
+    {
+        std::cout << "caught an error as expected: " << e.what() << '\n';
+    }
 }
